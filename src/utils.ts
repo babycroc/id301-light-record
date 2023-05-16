64ecbe0031d107773ba245a66e93eebb95dbe33f
Nicole Lee <nicolelee2001@gmail.com>
2023-05-17 03:25:52 +0900

    Organize repeatedly used functions into utils.js file

---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+import { Color } from "./types";
+
+export const convertToHexColor = (color: Color) => {
+  let hexColor = null;
+  switch (color) {
+    case Color.RED:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--red");
+      break;
+    case Color.ORANGE:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--orange");
+      break;
+    case Color.YELLOW:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--yellow");
+      break;
+    case Color.GREEN:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--green");
+      break;
+    case Color.BLUE:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--blue");
+      break;
+    case Color.PURPLE:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--purple");
+      break;
+    case Color.PINK:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--pink");
+      break;
+    case Color.NONE:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--white");
+      break;
+    default:
+      hexColor = getComputedStyle(document.body).getPropertyValue("--white");
+      break;
+  }
+  return hexColor;
+};
