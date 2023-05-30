8c624dd76dea8f6de1dcc8f1ae375070a4beb2d7
Nicole Lee <nicolelee2001@gmail.com>
2023-05-31 04:50:16 +0900

    Remove unused code

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,12 +1,4 @@
 import React from "react";
-import { PieOutlineSVG } from "../assets/svg/PieOutline";
-import { PieSVG } from "../assets/svg/Pie";
-import { styled } from "styled-components";
-
-const PieContainer = styled.div`
-  position: absolute;
-  top: 0;
-`;
 
 export const Settings: React.FC = () => {
   return <>Settings</>;
